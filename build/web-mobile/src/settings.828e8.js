f2f5af02dff3d652ae606f380a736cdd0f503e12
xu_yanfeng <xu_yanfeng@126.com>
2023-12-27 11:41:41 +0800

    build

---
+++ build/web-mobile/src/settings.828e8.js
@@ -0,0 +1,1 @@
+window._CCSettings={platform:"web-mobile",groupList:["default"],collisionMatrix:[[true]],hasResourcesBundle:true,hasStartSceneBundle:false,remoteBundles:[],subpackages:[],launchScene:"db://assets/db.fire",orientation:"",jsList:[],bundleVers:{internal:"d17e2",resources:"cc643",main:"98724"}};
